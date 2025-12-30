684c539ca3ce86b492cc47e56526510ee5569f0d
Minseong Jang <minseong.jang@kaist.ac.kr>
2025-12-30 13:12:33 +0000

    Simplify UART logic

--- src/uncore/uartPC16550D.sv
+++ src/uncore/uartPC16550D.sv
@@ -71,30 +71,15 @@ module uartPC16550D #(parameter UART_PRESCALE) (
   logic        DLAB; // Divisor Latch Access Bit (LCR bit 7)
 
   // Baud and rx/tx timing
-  logic                         baudpulse, txbaudpulse; // high one system clk cycle each baud/16 period
-  logic [16+UART_PRESCALE-1:0]  baudcount;
-  logic [3:0]                   txoversampledcnt;  // count oversampled-by-16
-  logic [3:0]                   txbitssent;
   statetype txstate;
 
-  // shift registers and FIFOs
-  logic [3:0]                   txbitsexpected;
-
   // transmit data
-  logic [7:0]                   TXHR, nexttxdata;
-  logic [11:0]                  txdata, txsr;
-  logic                         txnextbit, txhrfull, txsrfull;
-  logic                         txparity;
-
-  // control signals
-  logic                         evenparitysel;
+  logic                         txhrfull, txsrfull;
 
   // interrupts
   logic                         THRE, THRE_IP, squashTHRE_IP, prevSquashTHRE_IP, setSquashTHRE_IP, resetSquashTHRE_IP;
   logic                         intrpending;
   logic [2:0]                   intrID;
-
-  logic                         baudpulseComb;
 
   ///////////////////////////////////////////
   // Register interface (Table 1, note some are read only and some write only)
@@ -115,7 +100,7 @@ module uartPC16550D #(parameter UART_PRESCALE) (
       if (~MEMWb) begin
         /* verilator lint_off CASEINCOMPLETE */
         case (A)
-          UART_DLL_RBR: if (DLAB) DLL <= Din; // else TXHR <= Din; // TX handled in TX register/FIFO section
+          UART_DLL_RBR: if (DLAB) DLL <= Din; // TX handled in TX register/FIFO section
           UART_DLM_IER: if (DLAB) DLM <= Din; else IER <= Din[3:0];
           UART_IIR: FCR <= {Din[7:6], 2'b0, Din[3], 2'b0, Din[0]}; // Write only FIFO Control Register; 4:5 reserved and 2:1 self-clearing
           UART_LCR: LCR <= Din;
@@ -157,104 +142,39 @@ module uartPC16550D #(parameter UART_PRESCALE) (
     else Dout = 8'b0;
 
   ///////////////////////////////////////////
-  // Baud rate generator
-  // consider switching to same fixed-frequency reference clock used for TIME register
-  // prescale by factor of 2^UART_PRESCALE to allow for high-frequency reference clock
-  // Unlike PC16550D, this unit is hardwired with same rx and tx baud clock
-  // For example, with PCLK = 320 MHz, UART_PRESCALE = 5, DLM = 0, DLL = 65,
-  // 320 MHz system clock is divided by 65 x 2^5.  The UART clock 16x oversamples
-  // the data, so the baud rate is 320x10^6 / (65 x 2^5 x 16) = 9615 Hz, which is
-  // close enough to 9600 baud to stay synchronized over the duration of one character.
-  ///////////////////////////////////////////
-  always_ff @(posedge PCLK)
-    if (~PRESETn) begin
-      baudcount <= 1;
-      baudpulse <= 1'b0;
-    end else if (~MEMWb & DLAB & (A == 3'b0 | A == 3'b1)) begin
-      baudcount <= 1;
-    end else begin
-      // the baudpulse is too long by 2 clock cycles.
-      // This is cause baudpulse is registered adding 1 cycle and
-      // baudcount is reset when baudcount equals the threshold {DLM, DLL, UART_PRESCALE}
-      // rather than 1 less than that value.  Alternatively the reset value could be 1 rather
-      // than 0.
-      baudpulse <= baudpulseComb;
-      baudcount <= baudpulseComb ? 1 :  baudcount +1;
-    end
-
-  assign baudpulseComb = (baudcount == {DLM, DLL, {(UART_PRESCALE){1'b0}}});
-
-  assign txbaudpulse = baudpulse;
-
-  ///////////////////////////////////////////
   // transmit timing and control
   ///////////////////////////////////////////
 
   always_ff @(posedge PCLK)
     if (~PRESETn) begin
-      txoversampledcnt <= '0;
-      txstate          <= UART_IDLE;
-      txbitssent       <= '0;
+      txstate <= UART_IDLE;
     end else if ((txstate == UART_IDLE) & txsrfull) begin // start transmitting
-      txstate          <= UART_ACTIVE;
-      txoversampledcnt <= 4'b1;
-      txbitssent       <= '0;
-    end else if (txbaudpulse & (txstate == UART_ACTIVE)) begin
-      txoversampledcnt <= txoversampledcnt + 1'b1;
-      if (txnextbit) begin // transmit at end of phase
-        txbitssent <= txbitssent + 1'b1;
-        if (txbitssent == txbitsexpected) txstate <= UART_DONE;
-      end
+      txstate <= UART_ACTIVE;
+    end else if (txstate == UART_ACTIVE) begin
+      txstate <= UART_DONE;
     end else if (txstate == UART_DONE) begin
       txstate <= UART_IDLE;
     end
-
-  assign txbitsexpected = 4'd1 + (4'd5 + {2'b00, LCR[1:0]}) + {3'b000, LCR[3]} + 4'd1 + {3'b000, LCR[2]} - 4'd1; // start bit + data bits + (parity bit) + stop bit(s) - 1
-  assign txnextbit = txbaudpulse & (txoversampledcnt == 4'b0000);  // implies txstate = UART_ACTIVE
 
   ///////////////////////////////////////////
   // transmit holding register, shift register, FIFO
   ///////////////////////////////////////////
 
-  always_comb begin // compute value for parity and tx holding register
-    nexttxdata = TXHR; // pick from FIFO or holding register
-    case (LCR[1:0]) // compute parity from appropriate number of bits
-      2'b00: txparity = ^nexttxdata[4:0] ^ ~evenparitysel;
-      2'b01: txparity = ^nexttxdata[5:0] ^ ~evenparitysel;
-      2'b10: txparity = ^nexttxdata[6:0] ^ ~evenparitysel;
-      2'b11: txparity = ^nexttxdata[7:0] ^ ~evenparitysel;
-    endcase
-    case({LCR[3], LCR[1:0]}) // parity, data bits
-      // load up start bit (0), 5-8 data bits, 0-1 parity bits, 2 stop bits (only one sometimes used), padding
-      3'b000: txdata = {1'b0, nexttxdata[0], nexttxdata[1], nexttxdata[2], nexttxdata[3], nexttxdata[4], 6'b111111};                                                       // 5 data, no parity
-      3'b001: txdata = {1'b0, nexttxdata[0], nexttxdata[1], nexttxdata[2], nexttxdata[3], nexttxdata[4], nexttxdata[5], 5'b11111};                                         // 6 data, no parity
-      3'b010: txdata = {1'b0, nexttxdata[0], nexttxdata[1], nexttxdata[2], nexttxdata[3], nexttxdata[4], nexttxdata[5], nexttxdata[6], 4'b1111};                           // 7 data, no parity
-      3'b011: txdata = {1'b0, nexttxdata[0], nexttxdata[1], nexttxdata[2], nexttxdata[3], nexttxdata[4], nexttxdata[5], nexttxdata[6], nexttxdata[7], 3'b111};             // 8 data, no parity
-      3'b100: txdata = {1'b0, nexttxdata[0], nexttxdata[1], nexttxdata[2], nexttxdata[3], nexttxdata[4], txparity, 5'b11111};                                              // 5 data, parity
-      3'b101: txdata = {1'b0, nexttxdata[0], nexttxdata[1], nexttxdata[2], nexttxdata[3], nexttxdata[4], nexttxdata[5], txparity, 4'b1111};                                // 6 data, parity
-      3'b110: txdata = {1'b0, nexttxdata[0], nexttxdata[1], nexttxdata[2], nexttxdata[3], nexttxdata[4], nexttxdata[5], nexttxdata[6], txparity, 3'b111};                  // 7 data, parity
-      3'b111: txdata = {1'b0, nexttxdata[0], nexttxdata[1], nexttxdata[2], nexttxdata[3], nexttxdata[4], nexttxdata[5], nexttxdata[6], nexttxdata[7], txparity, 2'b11};    // 8 data, parity
-    endcase
-  end
-
   // registers & FIFO
   always_ff @(posedge PCLK)
     if (~PRESETn) begin
-      txhrfull <= 1'b0; txsrfull <= 1'b0; TXHR <= '0; txsr <= 12'hfff;
+      txhrfull <= 1'b0; txsrfull <= 1'b0;
     end else begin
       if (~MEMWb & A == 3'b000 & ~DLAB) begin // writing transmit holding register or fifo
-        TXHR     <= Din;
         txhrfull <= 1'b1;
         $write("%c",Din); // for testbench
       end
       if (txstate == UART_IDLE) begin // move data into tx shift register if available
         if (txhrfull) begin
-          txsr     <= txdata;
           txhrfull <= 1'b0;
           txsrfull <= 1'b1;
         end
       end else if (txstate == UART_DONE) txsrfull <= 1'b0; // done transmitting shift register
-      else if (txstate == UART_ACTIVE & txnextbit) txsr <= {txsr[10:0], 1'b1}; // shift txhr
     end
 
   ///////////////////////////////////////////
@@ -287,7 +207,6 @@ module uartPC16550D #(parameter UART_PRESCALE) (
   ///////////////////////////////////////////
 
   assign DLAB = LCR[7];
-  assign evenparitysel  = LCR[4];
 
 endmodule
 
